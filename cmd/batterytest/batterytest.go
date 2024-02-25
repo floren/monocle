48d029162aac7db4c26549d1f0534700967a1e93
John Floren <john@frodo.floren.lan>
2024-02-24 16:46:38 -0800

    Check in some basic utilities
    These are all just really lightly-hacked modifications of an example
    from the tinygo.org/x/bluetooth examples directory. Their stuff is
    under the BSD 3-clause license but they didn't have copyright headers
    on the example I used, so I dunno what to do.

---
+++ cmd/batterytest/batterytest.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+	"tinygo.org/x/bluetooth"
+)
+
+var (
+	serviceUUID = bluetooth.ServiceUUIDNordicUART
+	rxUUID      = bluetooth.CharacteristicUUIDUARTRX
+	txUUID      = bluetooth.CharacteristicUUIDUARTTX
+)
+
+var adapter = bluetooth.DefaultAdapter
+
+func main() {
+	// Enable BLE interface.
+	err := adapter.Enable()
+	if err != nil {
+		println("could not enable the BLE stack:", err.Error())
+		return
+	}
+
+	// The address to connect to. Set during scanning and read afterwards.
+	var foundDevice bluetooth.ScanResult
+
+	// Scan for NUS peripheral.
+	println("Scanning...")
+	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		if !result.AdvertisementPayload.HasServiceUUID(serviceUUID) {
+			return
+		}
+		foundDevice = result
+
+		// Stop the scan.
+		err := adapter.StopScan()
+		if err != nil {
+			// Unlikely, but we can't recover from this.
+			println("failed to stop the scan:", err.Error())
+		}
+	})
+	if err != nil {
+		println("could not start a scan:", err.Error())
+		return
+	}
+
+	// Found a device: print this event.
+	if name := foundDevice.LocalName(); name == "" {
+		print("Connecting to ", foundDevice.Address.String(), "...")
+		println()
+	} else {
+		print("Connecting to ", name, " (", foundDevice.Address.String(), ")...")
+		println()
+	}
+
+	// Found a NUS peripheral. Connect to it.
+	device, err := adapter.Connect(foundDevice.Address, bluetooth.ConnectionParams{})
+	if err != nil {
+		println("Failed to connect:", err.Error())
+		return
+	}
+
+	// Connected. Look up the Nordic UART Service.
+	println("Discovering service...")
+	services, err := device.DiscoverServices([]bluetooth.UUID{serviceUUID})
+	if err != nil {
+		println("Failed to discover the Nordic UART Service:", err.Error())
+		return
+	}
+	service := services[0]
+
+	// Get the two characteristics present in this service.
+	chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{rxUUID, txUUID})
+	if err != nil {
+		println("Failed to discover RX and TX characteristics:", err.Error())
+		return
+	}
+	rx := chars[0]
+	tx := chars[1]
+	// Enable notifications to receive incoming data.
+	err = tx.EnableNotifications(func(value []byte) {
+		println(string(value))
+	})
+	if err != nil {
+		println("Failed to enable TX notifications:", err.Error())
+		return
+	}
+	// send a ctrl-c + carriage return
+	sendUartCommand(rx, string([]byte{0x03, 0x0d}))
+
+	format := `import display, time, device
+# blank the screen
+display.show()
+# set the time
+time.time(%d)
+time.zone("-8:00")
+def gettime():
+    t=time.now()
+    return f'{t["month"]}/{t["day"]} {t["hour"]:02}:{t["minute"]:02}:{t["second"]:02}'
+
+while True:
+    print(gettime())
+    print(device.battery_level())
+    time.sleep(60)
+
+`
+
+	cmd := fmt.Sprintf(format, time.Now().Unix())
+	if err := sendUartCommand(rx, cmd); err != nil {
+		panic(err)
+	}
+	for {
+		time.Sleep(10 * time.Second)
+		if err := sendUartCommand(rx, "\r"); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func sendUartCommand(rx bluetooth.DeviceCharacteristic, commandString string) error {
+	// It expects carriage returns instead of newlines. Fine.
+	cmd := []byte(strings.ReplaceAll(commandString, "\n", "\r"))
+	for len(cmd) != 0 {
+		// Chop off up to 20 bytes from the sendbuf.
+		partlen := 20
+		if len(cmd) < 20 {
+			partlen = len(cmd)
+		}
+		part := cmd[:partlen]
+		cmd = cmd[partlen:]
+		_, err := rx.WriteWithoutResponse(part)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module monocle
+
+go 1.21.5
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/muka/go-bluetooth v0.0.0-20221213043340-85dc80edc4e1 // indirect
+	github.com/saltosystems/winrt-go v0.0.0-20240110120258-ad49e9790c38 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tinygo-org/cbgo v0.0.4 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+	tinygo.org/x/bluetooth v0.8.1-0.20240221113924-0087e0549b0b // indirect
+)
